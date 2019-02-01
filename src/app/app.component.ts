f23294f785391446da05acee0e875682215583ab
Lourens Schep <lourensschep@gmail.com>
2019-01-31 20:49:29 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Scales,
+  Axes,
+  Plots,
+  Dataset,
+  Components,
+  Interactions
+} from 'plottable';
+import { SamplesApiService } from './services';
+import { Sample } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('plottablejs') private svgEl: ElementRef;
   title = 'app';
+
+  constructor(private api: SamplesApiService) {}
+
+  ngOnInit() {
+    this.api.getAll().subscribe(samples => {
+      this.makeBasicChart(samples);
+    });
+  }
+
+  makeBasicChart(data: Sample[]) {
+    const xScale = new Scales.Time();
+    const yScale = new Scales.Linear();
+
+    const xAxis = new Axes.Time(xScale, 'bottom');
+    const yAxis = new Axes.Numeric(yScale, 'left');
+
+    const plot = new Plots.Line();
+    plot.x((d: Sample) => {
+      return d.dateTime;
+    }, xScale);
+    plot
+      .y((d: Sample, _index: number, dataset: Dataset) => {
+        return d[dataset.metadata().fieldKey];
+      }, yScale)
+      .attr('stroke', function(_d, _i, ds) {
+        return colorScale.scale(ds.metadata().fieldKey);
+        // return ds.metadata().color;
+      });
+
+    const tempDataset = new Dataset(data, {
+      fieldKey: 'temperature',
+      color: '#34b24c'
+    });
+    plot.addDataset(tempDataset);
+    const moistDataset = new Dataset(data, {
+      fieldKey: 'moisture',
+      color: '#ffa500'
+    });
+    plot.addDataset(moistDataset);
+    const humDataset = new Dataset(data, {
+      fieldKey: 'humidity',
+      color: '#551a8b'
+    });
+    plot.addDataset(humDataset);
+
+    const colorScale = new Scales.Color();
+    const legend = new Components.Legend(colorScale).maxEntriesPerRow(3);
+
+    const chart = new Components.Table([
+      [null, legend],
+      [yAxis, plot],
+      [null, xAxis]
+    ]);
+
+    const pzi = new Interactions.PanZoom();
+    pzi.addXScale(xScale);
+    pzi.addYScale(yScale);
+    pzi.attachTo(plot);
+    pzi.enabled(true);
+
+    chart.renderTo(this.svgEl.nativeElement);
+  }
 }
---
+++ src/app/models/fake-samples.ts
@@ -0,0 +1,62 @@
+import { Sample } from './sample';
+
+const FakeSamples: Sample[] = [
+  {
+    uuid: '35cfdf76-3c9a-40d9-baad-1b1094ea02c5',
+    date_time: '2018-06-01 00:26:09.082970195 UTC',
+    moisture: 3.555,
+    humidity: 123.213,
+    temperature: 123.23
+  },
+  {
+    uuid: '311c72df-9a73-4678-aa24-79c0595eff26',
+    date_time: '2018-06-01 00:26:51.036899939 UTC',
+    moisture: 3.555,
+    humidity: 123.213,
+    temperature: 123.23
+  },
+  {
+    uuid: '308a1d79-fafb-456c-a526-5ca04ef7a9a2',
+    date_time: '2018-06-09 16:56:59.708276477 UTC',
+    moisture: 996.0,
+    humidity: 39.88,
+    temperature: 22.79
+  },
+  {
+    uuid: '20a22932-9c46-4b86-acae-d3efff9280c0',
+    date_time: '2018-06-09 16:57:08.331377635 UTC',
+    moisture: 996.0,
+    humidity: 39.85,
+    temperature: 22.78
+  },
+  {
+    uuid: '3f550d27-4aec-44c1-9fca-9cfa243a628f',
+    date_time: '2018-06-09 16:57:16.954777288 UTC',
+    moisture: 996.0,
+    humidity: 39.84,
+    temperature: 22.8
+  },
+  {
+    uuid: '56b77370-5c40-4e42-a8ae-3e46d54fcf7c',
+    date_time: '2018-06-09 16:57:25.572361933 UTC',
+    moisture: 996.0,
+    humidity: 39.81,
+    temperature: 22.78
+  },
+  {
+    uuid: '07bb85a3-4997-4fb8-841e-32ebcee6aa33',
+    date_time: '2018-06-09 16:57:34.195393030 UTC',
+    moisture: 996.0,
+    humidity: 39.82,
+    temperature: 22.78
+  },
+  {
+    uuid: 'd2825df0-2726-40ec-b0a1-18a29aa0ce65',
+    date_time: '2018-06-09 16:57:42.810378331 UTC',
+    moisture: 996.0,
+    humidity: 39.85,
+    temperature: 22.79
+  }
+];
+
+export { FakeSamples };
